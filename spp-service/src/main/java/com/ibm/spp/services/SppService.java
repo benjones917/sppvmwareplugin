1736a4456919d92c250245e279f54d37ed0a8f3e
Ben Jones <benjones917@gmail.com>
2017-11-26 11:22:57 -0500

    Changes to store spp reg info

--- spp-service/src/main/java/com/ibm/spp/services/SppService.java
+++ spp-service/src/main/java/com/ibm/spp/services/SppService.java
@@ -2,4 +2,8 @@ package com.ibm.spp.services;
 
 public interface SppService {
 	String register(String registrationInfo);
+
+	String getSlaPolicies();
+
+	String getSlaPoliciesForVM();
 }
--- spp-service/src/main/java/com/ibm/spp/services/SppServiceImpl.java
+++ spp-service/src/main/java/com/ibm/spp/services/SppServiceImpl.java
@@ -1,7 +1,12 @@
 package com.ibm.spp.services;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -9,21 +14,16 @@ import java.util.Base64;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 
@@ -35,20 +35,89 @@ import com.ibm.spp.domain.SppUrls;
 public class SppServiceImpl implements SppService {
 
 	private static CloseableHttpClient createAcceptSelfSignedCertificateClient()
-            throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+			throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
 
-        SSLContext sslContext = SSLContextBuilder.create().loadTrustMaterial(new TrustSelfSignedStrategy()).build();
-        HostnameVerifier allowAllHosts = new NoopHostnameVerifier();
-        SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, allowAllHosts);
-        return HttpClients.custom().setSSLSocketFactory(connectionFactory).build();
-    }
-	
+		SSLContext sslContext = SSLContextBuilder.create().loadTrustMaterial(new TrustSelfSignedStrategy()).build();
+		HostnameVerifier allowAllHosts = new NoopHostnameVerifier();
+		SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, allowAllHosts);
+		return HttpClients.custom().setSSLSocketFactory(connectionFactory).build();
+	}
+
 	@Override
 	public String register(String registrationInfo) {
 		RegistrationInfo regInfo = new Gson().fromJson(registrationInfo, RegistrationInfo.class);
 		SppSession session = sppLogIn(regInfo.getSppHost(), regInfo.getSppUser(), regInfo.getSppPass());
 		sppLogOut(regInfo.getSppHost(), session);
+		setSppRegistrationInfo(registrationInfo);
+		regInfo = getSppRegistrationInfo();
 		return "Registration succesful";
+	}
+
+	@Override
+	public String getSlaPolicies() {
+		return null;
+	}
+	
+	@Override 
+	public String getSlaPoliciesForVM() {
+		return null;
+	}
+
+	public String getRegistrationInfoPath() {
+		String path;
+		if (SystemUtils.IS_OS_WINDOWS) {
+			path = System.getenv("PROGRAMDATA") + "\\VMWare\\vCenterServer\\spp";
+		} else if (SystemUtils.IS_OS_MAC) {
+			path = "/var/lib/spp";
+		} else {
+			path = "/storage/spp";
+		}
+		return path;
+	}
+	
+	public String getRegistrationInfoFilePath() {
+		String path;
+		if (SystemUtils.IS_OS_WINDOWS) {
+			path = System.getenv("PROGRAMDATA") + "\\VMWare\\vCenterServer\\spp\\regInfo.config";
+		} else if (SystemUtils.IS_OS_MAC) {
+			path = "/var/lib/spp/regInfo.config";
+		} else {
+			path = "/storage/spp/regInfo.config";
+		}
+		return path;
+	}
+	
+	public void setSppRegistrationInfo(String regInfo) {
+		String path = getRegistrationInfoPath();
+		String filePath = getRegistrationInfoFilePath();
+		try {
+			File info = new File(path);
+			File config = new File(filePath);
+			if(!info.exists()) {
+				info.mkdirs();
+				config.createNewFile();
+			}
+			BufferedWriter writer = new BufferedWriter(new FileWriter(config));
+			writer.write(regInfo);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public RegistrationInfo getSppRegistrationInfo() {
+		String filePath = getRegistrationInfoFilePath();
+		String config;
+		try {
+			FileReader reader = new FileReader(filePath);
+			BufferedReader br = new BufferedReader(reader);
+			config = br.readLine();
+			br.close();
+			return new Gson().fromJson(config, RegistrationInfo.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public SppSession sppLogIn(String host, String user, String pass) {
@@ -69,7 +138,7 @@ public class SppServiceImpl implements SppService {
 		}
 		return new SppSession("Failure to create session");
 	}
-	
+
 	public String sppLogOut(String host, SppSession session) {
 		String sppSesUrl = host + SppUrls.sppSessionUrl;
 		String sesIdHdr = session.getSessionId();
@@ -78,7 +147,7 @@ public class SppServiceImpl implements SppService {
 			HttpUriRequest request = RequestBuilder.delete().setUri(sppSesUrl).build();
 			request.setHeader("X-Endeavour-Sessionid", sesIdHdr);
 			HttpResponse response = httpclient.execute(request);
-			if(response.getStatusLine().getStatusCode() == 204) {
+			if (response.getStatusLine().getStatusCode() == 204) {
 				return "Log out succesful";
 			}
 		} catch (Exception e) {
